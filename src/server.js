8e2b445658f474cd4e51f6f34b6da111b1c7e07c
Nordward IT <dev@nordward.com>
2026-03-31 11:35:47 +0200

    Restore cookieless login embed redirect

--- src/server.js
+++ src/server.js
@@ -684,10 +684,12 @@ async function getCookielessEmbedUrl(
   targetUrl.searchParams.set("embed_domain", embedDomain);
   targetUrl.searchParams.set("embed_navigation_token", tokens.navigationToken);
 
+  const targetUri = encodeURIComponent(
+    `${targetUrl.pathname}${targetUrl.search}${targetUrl.hash}`
+  );
+
   return {
-    // Return the direct cookieless embed URL to avoid the /login/embed redirect,
-    // which sends X-Frame-Options: SAMEORIGIN and can confuse kiosk validators.
-    url: targetUrl.toString(),
+    url: `${targetUrl.origin}/login/embed/${targetUri}?embed_authentication_token=${encodeURIComponent(tokens.authenticationToken)}`,
     sessionReferenceToken: tokens.sessionReferenceToken,
     apiToken: tokens.apiToken,
     navigationToken: tokens.navigationToken,
